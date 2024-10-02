7b3da7310c8e5b0ec39bcf49d4e5dfffff9ad2fb
EngineerRohit01 <rohitkusv@gmail.com>
2024-10-02 14:17:20 +0530

    Service finished

--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -3,11 +3,28 @@ const app = express();
 const dotenv = require("dotenv");
 const cron = require("node-cron");
 const dbConnection = require("./utils/db");
+const {
+  sendDetailsProspectEmail,
+} = require("./EmailServices/sendDetailsProspect");
+const {
+  sendEligibilityEmail,
+} = require("./EmailServices/sendEligibilityEmail");
+const {
+  sendBloodDonationReminder,
+} = require("./EmailServices/sendBloodDonationReminder");
+const {
+  sendDonorDetailsEmail,
+} = require("./EmailServices/sendDonorDetailsEmail");
 dotenv.config();
 
 //SCHEDULE TASK
 const run = () => {
-  cron.schedule("* * * * * *", () => {});
+  cron.schedule("* * * * * *", () => {
+    sendDetailsProspectEmail();
+    sendEligibilityEmail();
+    sendBloodDonationReminder();
+    sendDonorDetailsEmail();
+  });
 };
 
 //SERVER
